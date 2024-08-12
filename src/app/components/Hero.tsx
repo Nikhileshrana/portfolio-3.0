d255b4ce017aeebe818b3db1d9ddf568f915310f
Nikhilesh Rana <Nikhileshrana@gmail.com>
2024-08-13 01:18:32 +0530

    Fixesss

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -43,23 +43,21 @@ export function Hero(props: any) {
                 {props.button1}
               </button>
             </Link>
-            <Link href="#featuredsection">
+            {/* <Link href="#featuredsection">
               <button className="w-40 h-10 rounded-xl bg-white text-black border border-black  text-sm">
                 {props.button2}
               </button>
-            </Link>
-          </div>
+            </Link> */}
 
-
-          <Drawer>
+<Drawer>
             <DrawerTrigger>
 
 
-            <div className="m-4 bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6  text-white inline-block">
+            <div className= "bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6  text-white inline-block">
   <span className="absolute inset-0 overflow-hidden rounded-full">
     <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
   </span>
-  <div className="h-12 w-full relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 ">
+  <div className="h-10 w-full relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 ">
     <span>
       Ask AI Nikhil
     </span>
@@ -102,6 +100,13 @@ export function Hero(props: any) {
               </DrawerFooter>
             </DrawerContent>
           </Drawer>
+
+
+
+          </div>
+
+
+
 
         </div>
 
